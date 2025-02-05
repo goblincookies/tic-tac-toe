8b1b79cb1140de026cf319483bbca98ab6f52f0e
goblincookies <goblincookiebox@gmail.com>
2025-02-05 12:45:41 -0500

    has core game logic

--- ttt.js
+++ ttt.js
@@ -6,11 +6,59 @@ let playingGame = true;
 
 
 const theGame = ( function() {
-    let game=[];
+    let game=new Array(9);
+    let winningUser = -1;
+    let winningCells = [];
+    game.fill(-1);
+    // [0,1,0,-1,-1]
+    const clamp = function (val) {
+        val = Math.max( 0, Math.min( val, game.length));
+        return val;
+    };
     // is valid cell
+    const isValid  = function (slot) {
+        slot = clamp(slot);
+        return (game[slot] < 0);
+    };
     // add to cell
-    // clear cell
+    const addToCell = function ( user, slot ) {
+        slot = clamp(slot);
+        game[slot] = user;
+    };
     // winner check
+    const hasWinner = function () {
+        let winner = false;
+        for (let i = 0; i < 3; i++) {
+            // vert check
+            if (game[i]>=0 && game[i] == game[i+3] == game[i+6] ) {
+                winningCells = [i, i+3, i+6];
+                winningUser = game[i];
+                winner = true;
+            };
+            // horiz check
+            if (game[(i*3)]>=0 && game[(i*3)] == game[(i*3)+1] == game[(i*3)+2] ) {
+                winningCells = [(i*3), (i*3)+1, (i*3)+2];
+                winningUser = game[(i*3)];
+                winner = true;
+            };
+        }
+        if (game[0]>=0 && game[0] == game[4] == game[8] ) {
+            winningCells = [0, 4, 8];
+            winningUser = game[0];
+            winner = true;
+        };
+        if (game[2]>=0 && game[2] == game[4] == game[6] ) {
+            winningCells = [2, 4, 6];
+            winningUser = game[2];
+            winner = true;
+        };
+        return winner;
+    };
+    // winner
+    const getWinner = () => winningUser;
+    const getWinningCells = () => winningCells;
+
+    return {isValid, addToCell, hasWinner, getWinner, getWinningCells};
 })();
 
 const setStatus = (function() {
@@ -34,8 +82,20 @@ const setActiveUser = (function() {
     }
 })();
 
+
 setStatus("Hello");
 setStatus("O Turn");
 setActiveUser(user.CIRCLE);
+console.log(theGame.isValid(0));
+theGame.addToCell(user.CIRCLE, 0);
+theGame.addToCell(user.CIRCLE, 1);
+theGame.addToCell(user.CIRCLE, 2);
 
-// accept click
+console.log(theGame.isValid(0));
+console.log(theGame.hasWinner());
+console.log( theGame.getWinner() == 0 ? "CROSS" : "CIRCLE"  );
+console.log(theGame.getWinningCells());
+
+
+// console.log(theGame.isValid(5));
+
